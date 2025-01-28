65efd016ba8f42b2cc495719a38b5e9bc5fccbc0
Aditya <aditya.i.keshava@gmail.com>
2025-01-28 16:29:50 +0530

    sign-up api route

--- api/index.js
+++ api/index.js
@@ -2,16 +2,19 @@ import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import router from './routes/user.route.js';
-dotenv.config();
+import authrouter from './routes/auth.route.js';
+dotenv.config ();
 mongoose.connect(process.env.MONGO).then(()=>{
     console.log('connected to mongodb');
 }).catch((err)=>{
     console.log(err);
-})
+});
 const app=express();
+app.use(express.json());
 app.listen(3000,()=>{
     console.log("server is running on port 3000");
 }
 );
 
 app.use("/api/user",router);
+app.use("/api/auth",authrouter);
---
+++ api/routes/auth.route.js
@@ -0,0 +1,5 @@
+import express from 'express'
+import { signup } from '../controllers/auth.controller.js';
+const authrouter=express.Router();
+authrouter.post("/signup",signup)
+export default authrouter;
