fadf1f15cc6d25bf5a0824e7888032ad7f78b15d
Aditya <aditya.i.keshava@gmail.com>
2025-01-28 16:48:34 +0530

    middleware and function to handle possible errors

--- api/index.js
+++ api/index.js
@@ -18,3 +18,12 @@ app.listen(3000,()=>{
 
 app.use("/api/user",router);
 app.use("/api/auth",authrouter);
+app.use((err,req,res,next)=>{
+const statuscode=err.statuscode||500;
+const message=err.message||'Internal Server Error';
+return res.status(statuscode).json({
+    success:"false",
+    statuscode,
+    message
+});
+});
